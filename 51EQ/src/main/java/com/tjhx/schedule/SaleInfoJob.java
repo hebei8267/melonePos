4e467f1c643e8e203d17f8f4428cd15081e57944
hebei8267 <he__bei@126.com>
2014-12-02 17:38:30 +0800

    333

--- 51EQ/src/main/java/com/tjhx/schedule/SaleInfoJob.java
+++ 51EQ/src/main/java/com/tjhx/schedule/SaleInfoJob.java
@@ -3,6 +3,8 @@
  */
 package com.tjhx.schedule;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import com.tjhx.service.info.SalesDayTotalSupManager;
 import com.tjhx.service.info.SalesMonthTotalItemManager;
 import com.tjhx.service.info.SalesWeekTotalGoodsManager;
 import com.tjhx.service.info.StoreDetailManager;
+import com.tjhx.service.info.SupplierManager;
 
 /**
  * @author he_bei
@@ -35,6 +38,8 @@ public class SaleInfoJob implements IJob {
 	private SalesDayTotalGoodsManager salesDayTotalGoodsManager;
 	@Autowired
 	private SalesWeekTotalGoodsManager salesWeekTotalGoodsManager;
+	@Resource
+	private SupplierManager supplierManager;
 
 	/*
 	 * (non-Javadoc)
@@ -43,12 +48,16 @@ public class SaleInfoJob implements IJob {
 	 */
 	@Override
 	public void execute() throws Exception {
+		// 同步百威数据库供应商信息
+		logger.info("同步百威数据库供应商信息 Begin");
+		supplierManager.bwDataSyn();
+		logger.info("同步百威数据库供应商信息 End");
+
 		logger.info("取得百威系统商品信息 Begin");
 		// 取得百威系统各门店日销售信息
 		goodsManager.bwDataSyn();
 		logger.info("取得百威系统商品信息 End");
-		
-		
+
 		logger.info("清理t_store_detail Begin");
 		// 清理t_store_detail(数据量过大,其数据已汇总到t_store_day_total)
 		// storeDetailManager.initTable();
@@ -64,9 +73,7 @@ public class SaleInfoJob implements IJob {
 		// 计算门店库存合计信息
 		storeDetailManager.calOrgStoreDayTotal();
 		logger.info("计算门店库存合计信息 End");
-		
-		
-		
+
 		logger.info("取得百威系统各门店日销售信息(按商品分类) Begin");
 		// 取得百威系统各门店日销售信息
 		salesDayTotalItemManager.getOrgSalesDayTotal();
@@ -78,13 +85,11 @@ public class SaleInfoJob implements IJob {
 		salesMonthTotalItemManager.calOrgSalesMonthTotal();
 		logger.info("门店月销售计算(按商品分类) End");
 
-		
 		logger.info("取得百威系统各门店日销售信息(按供应商) Begin");
 		// 取得百威系统各门店日销售信息(按供应商)
 		salesDayTotalSupManager.getOrgSalesDayTotal();
 		logger.info("取得百威系统各门店日销售信息(按供应商) End");
-		
-		
+
 		logger.info("取得百威系统各门店日销售信息(按商品) Begin");
 		// 取得百威系统各门店日销售信息
 		salesDayTotalGoodsManager.getOrgSalesDayTotal();
