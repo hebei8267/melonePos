13ced190edb3d68370e2af966ca1f08326d5da57
hebei8267 <he__bei@126.com>
2015-03-17 15:18:46 +0800

    2222

---
+++ 51EQ/src/test/java/test/com/tjhx/service/info/BudgetSubjectManagerTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package test.com.tjhx.service.info;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springside.modules.test.spring.SpringTransactionalTestCase;
+
+import com.tjhx.dao.info.BudgetSubjectJpaDao;
+import com.tjhx.entity.info.BudgetSubject;
+
+/**
+ * @author he_bei
+ * 
+ */
+public class BudgetSubjectManagerTest extends SpringTransactionalTestCase {
+	@Resource
+	private BudgetSubjectJpaDao budgetSubjectJpaDao;
+
+	@Test
+	@Rollback(false)
+	public void saveNewBank() {
+		BudgetSubject sub = new BudgetSubject();
+
+		// 预算科目名称
+		sub.setSubName("根预算科目");
+		// 排序
+		sub.setSortIndex(1);
+
+		budgetSubjectJpaDao.save(sub);
+	}
+}
