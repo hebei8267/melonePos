db31356e529ba20918a69c4becdf5afd269424b9
hebei8267 <he__bei@126.com>
2015-08-15 14:48:26 +0800

    3333

--- 51EQ/src/test/java/test/com/tjhx/service/job/AAA.java
+++ 51EQ/src/test/java/test/com/tjhx/service/job/AAA.java
@@ -14,6 +14,7 @@ import org.springside.modules.test.spring.SpringTransactionalTestCase;
 
 import com.tjhx.service.accounts.CashDailyManager;
 import com.tjhx.service.info.SalesOrdersDayTotalManager;
+import com.tjhx.service.info.SalesWeekTotalGoodsManager2;
 
 /**
  * @author he_bei
@@ -23,10 +24,16 @@ public class AAA extends SpringTransactionalTestCase {
 	private static Logger logger = LoggerFactory.getLogger(AAA.class);
 	@Autowired
 	private SalesOrdersDayTotalManager salesOrdersDayTotalManager;
+	@Autowired
+	private CashDailyManager cashDailyManager;
+	@Autowired
+	private SalesWeekTotalGoodsManager2 salesWeekTotalGoodsManager2;
 
 	@Test
 	@Rollback(false)
 	public void tt() throws ParseException {
-		salesOrdersDayTotalManager.bwDataSyn();
+		logger.info("同步支付宝销售额 Begin");
+		cashDailyManager.synZFBSaleAmt();
+		logger.info("同步支付宝销售额 Begin");
 	}
 }
